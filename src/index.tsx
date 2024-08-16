6bb8949931fd807dec2815b7e19e11e573ce005d
RenanGeorgio <regeo9@hotmail.com>
2024-08-16 19:55:51 -0300

    feat
    correção para rodar como aplicação React (antes estava como Nextjs)

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "global.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>    
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
