8c2ef640ff5f23fb449722db5ea8e0659f725acc
RenanGeorgio <regeo9@hotmail.com>
2024-08-26 14:55:44 -0300

    fix
    force reverse merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 // import Sidebar from "./components/side-bar";
 // import { SidebarProvider } from "./contexts/SidebarContext";
@@ -7,8 +7,11 @@ import Faq from "./pages/faq/faqPage";
 import User from "./pages/user/userPage";
 
 function App() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div className="app-container">
+      {/*<Sidebar expanded={expanded} setExpanded={setExpanded} />*/}
       <Routes>
         <Route path="/" element={<Chat />} />
         <Route path="/faq" element={<Faq />} />
