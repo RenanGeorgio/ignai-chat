baffca194e082ba7e5b103c102c45140500d2a5b
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2024-08-30 14:10:24 -0300

    fix: ajustes faq page

--- src/components/bug.tsx
+++ src/components/bug.tsx
@@ -1,37 +1,55 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import styles from "../style/bug.module.css";
 
 export type BugType = {
   className?: string;
 };
 
-const BugComponets: FunctionComponent<BugType> = ({ className = "" }) => {
+const BugComponents: FunctionComponent<BugType> = ({ className = "" }) => {
+  const [tickets, setTickets] = useState<string[]>([
+    "Erro ao coletar os dados",
+    "Erro no buscador"
+  ]);
+
+  const handleAddTicket = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (event.target.value.trim() !== "") {
+      setTickets([...tickets, event.target.value]);
+      event.target.value = ""; 
+    }
+  };
+
   return (
     <div className={[styles.bugContainer, className].join(" ")}>
       <div className={styles.bugHeader}>
         <h3 className={styles.bugTitle}>Relatar Bug</h3>
+      </div>
+      <div className={styles.bugInputContainer}>
+        <textarea
+          className={styles.bugInput}
+          placeholder="Escreva seu problema..."
+          onBlur={handleAddTicket}
+        />
+        <button className={styles.uploadButton} aria-label="Upload file">
+          Enviar
+        </button>
       </div>
       <div className={styles.bugBody}>
         <div className={styles.bugHeader}>
           <h3 className={styles.bugTitle}>Lista de Tickets</h3>
         </div>
         <div className={styles.containerBug}>
-          <div className={styles.bugItem}>          
-            <div className={styles.statusBug}>
-              <p className={styles.bug}>{`1. Erro ao coletar os dados`}</p>
-              <span className={styles.emojiBug}>🎧</span>  
+          {tickets.map((ticket, index) => (
+            <div className={styles.bugItem} key={index}>
+              <div className={styles.statusBug}>
+                <p className={styles.bug}>{`${index + 1}. ${ticket}`}</p>
+                <span className={styles.emojiBug}>✔️</span>
+              </div>
             </div>
-          </div>
-          <div className={styles.bugItem}>          
-            <div className={styles.statusBug}>
-              <p className={styles.bug}>{`2. Erro no buscador`}</p>
-              <span className={styles.emojiBug}>🎧</span>  
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default BugComponets;
+export default BugComponents;
