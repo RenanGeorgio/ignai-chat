257bbd98fb2fdefaeb797f2f53982b34dcd18524
RenanGeorgio <regeo9@hotmail.com>
2024-09-02 19:33:55 -0300

    up
    botoes com função de automação de atendimento

--- src/components/Queue/index.tsx
+++ src/components/Queue/index.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useState } from "react";
-import { Switch, Stack, FormGroup, FormControlLabel, Button } from "@mui/material";
-import { MessageCircleIcon } from "../../assets/icons";
+import React, { FunctionComponent, useState, useRef } from "react";
+import { Switch, Stack, FormGroup, FormControlLabel, Button, ButtonGroup, Popper, Grow, Paper, ClickAwayListener } from "@mui/material";
+import { MessageCircleIcon, SettingIcon } from "../../assets/icons";
 
 import styles from "./queue.module.css";
 
@@ -8,20 +8,106 @@ export type QueueItemsType = {
   className?: string;
 };
 
+type GrowProps = {
+  TransitionProps: any 
+  placement: string
+};
+
 const QueueComponent: FunctionComponent<QueueItemsType> = ({ className = "" }) => {
-  const [checked, setChecked] = useState(true);
+  const anchorRef = useRef<HTMLDivElement>(null);
+
+  const [checked, setChecked] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(false);
+  const [manual, setManual] = useState<boolean>(true);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
+  };
+
+  const handleChangeManualMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setManual(event.target.checked);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen: boolean) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    if ((anchorRef.current) && (anchorRef.current.contains(event.target as HTMLElement))) {
+      return;
+    }
+
+    setOpen(false);
   };
 
   return (
     <div className={[styles.queueItems, className].join(" ")}>
       <div className={styles.queueHeadings}>
         <Stack direction="row" spacing={2}>
-          <Button variant="contained" endIcon={<MessageCircleIcon />}>
-            Send
-          </Button>
+          <ButtonGroup
+            variant="contained"
+            ref={anchorRef}
+            aria-label="Button group with a nested menu"
+          >
+            <Button 
+              size="medium" 
+              variant="contained" 
+              disabled={false}
+              endIcon={<MessageCircleIcon />}
+              onClick={() => {
+                alert('clicked');
+              }}
+            >
+              Send
+            </Button>
+            <Button
+              size="small"
+              aria-controls={open ? 'split-button-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-label="select merge strategy"
+              aria-haspopup="menu"
+              onClick={handleToggle}
+            >
+              <SettingIcon />
+            </Button>
+          </ButtonGroup>
+          <Popper
+            sx={{ zIndex: 1 }}
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }: GrowProps) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === 'bottom' ? 'center top' : 'center bottom',
+                }}
+              >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <FormGroup>
+                    <FormControlLabel 
+                      control={
+                        <Switch
+                          checked={manual}
+                          onChange={handleChangeManualMode}
+                          inputProps={{ 'aria-label': 'controlled' }}
+                          defaultChecked 
+                          size="small"
+                        />
+                      } 
+                      label={manual ? "manual" : "auto"}
+                    />
+                  </FormGroup>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+            )}
+          </Popper>
           <FormGroup>
             <FormControlLabel
               control={
