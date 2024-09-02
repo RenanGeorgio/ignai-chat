7fe8b0b1f233b0d5b75d097dcef1cb1d078ebb58
RenanGeorgio <regeo9@hotmail.com>
2024-09-02 17:30:52 -0300

    feat
    types

---
+++ declarations.d.ts
@@ -0,0 +1,34 @@
+declare module '*.module.css' {
+    const content: any;
+    export default content;
+}
+
+declare module '*.css' {
+    const content: any;
+    export default content;
+}
+
+declare module '*.scss' {
+    const content: any;
+    export default content;
+}
+
+declare module '*.svg' {
+    export const content: any;
+}
+
+declare module '*.css' {
+    export const content: any;
+}
+
+declare module '*.mp4' {
+    export const content: any;
+}
+
+declare module '*.png' {
+    export const content: any;
+}
+
+declare module '*.mjs' {
+    export const content: any;
+}
