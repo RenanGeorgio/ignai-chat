b838f2bc245c284cb081830a655406c57e7898b9
RenanGeorgio <regeo9@hotmail.com>
2024-09-18 14:35:54 -0300

    up

--- src/libs/token.ts
+++ src/libs/token.ts
@@ -16,3 +16,19 @@ export function getClientToken(ctx?: any) {
 
   return AuthApi;
 }
+
+export function getClientAuthToken(ctx?: any) {
+  const { 'auth.token': token } = parseCookies(ctx);
+
+  AuthApi.interceptors.request.use((config: any) => {
+    return config;
+  })
+
+  if (token) {
+    if (AuthApi.defaults.headers !== null){
+      AuthApi.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+  }
+
+  return AuthApi;
+}
