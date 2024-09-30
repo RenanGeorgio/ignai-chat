1dc8ad5dcb3943b762e05429e302c06b5ca2b4c5
Renan Georgio <43246444+RenanGeorgio@users.noreply.github.com>
2024-09-30 02:48:31 -0300

    Update ChatContext.tsx

--- src/contexts/chat/ChatContext.tsx
+++ src/contexts/chat/ChatContext.tsx
@@ -1,10 +1,7 @@
-import { createContext } from 'react';
-import { ChatContextType, type InactiveChatsContextType } from './types';
+import { createContext } from "react";
+import { ChatContextType, type InactiveChatsContextType } from "./types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const ChatContext = createContext<ChatContextType>(
-  {} as ChatContextType,
-);
-export const InactiveChatsContext = createContext<InactiveChatsContextType>(
-  {} as InactiveChatsContextType,
-);
+export const ChatContext = createContext<ChatContextType>({} as ChatContextType);
+
+export const InactiveChatsContext = createContext<InactiveChatsContextType>({} as InactiveChatsContextType);
