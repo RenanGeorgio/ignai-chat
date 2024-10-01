696419f47e7bbe3dfc603bd4f0d04e1f26dc8e65
RenanGeorgio <regeo9@hotmail.com>
2024-10-01 14:57:49 -0300

    test

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,16 +1,16 @@
 import { FunctionComponent } from "react";
 
-import { useChat } from "../contexts/chat/hooks";
+//import { useChat } from "../contexts/chat/hooks";
 import QueueComponent from "../components/Queue/index";
 import TicketsComponent from "../components/Tickets/index";
-import ChatComponent from "../components/Chat/index";
+//import ChatComponent from "../components/Chat/index";
 //import VoiceCall from "../components/Call/voice-call";
 import { Phone } from "../components/Call/Phone";
 
 import "../styles/chat.css";
 
 const Chat: FunctionComponent = () => {
-  const { currentChat } = useChat();
+  //const { currentChat } = useChat();
   
   return (
     <div className="dashboard">
@@ -23,12 +23,7 @@ const Chat: FunctionComponent = () => {
         <section className="content">
           <div className="container">
             <div className="separador">
-              {currentChat ? (
-                <ChatComponent />
-                ) : (
-                <Phone />
-                )
-              }
+            < Phone />
             </div>
           </div>
         </section>
