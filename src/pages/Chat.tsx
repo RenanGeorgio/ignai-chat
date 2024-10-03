b832c159934069b699a667e516114aecd531c276
RenanGeorgio <regeo9@hotmail.com>
2024-10-03 15:10:19 -0300

    fix

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,13 +1,13 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent } from "react";
 
-import { useChat } from '../contexts/chat/hooks';
-import QueueComponent from '../components/Queue/queue';
-import TicketsComponent from '../components/Tickets/index';
-import ChatComponent from '../components/Chat/index';
+import { useChat } from "../contexts/chat/hooks";
+import QueueComponent from "../components/Queue/queue";
+import TicketsComponent from "../components/Tickets/index";
+import ChatComponent from "../components/Chat/index";
 //import VoiceCall from "../components/Call/voice-call";
-import { Phone } from '../components/Call/Phone';
+import { Phone } from "../components/Call/Phone";
 
-import '../styles/chat.css';
+import "../styles/chat.css";
 
 const Chat: FunctionComponent = () => {
   const { currentChat } = useChat();
@@ -35,6 +35,6 @@ const Chat: FunctionComponent = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Chat;
