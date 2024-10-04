f4adff444262437ba004b1c64621339d78cb00d3
RenanGeorgio <regeo9@hotmail.com>
2024-10-04 20:21:00 -0300

    feat
    dashboard

---
+++ src/components/Communication/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+
+import CustomDatePicker from "./CustomDatePicker";
+
+export default function Header() {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        display: { xs: 'none', md: 'flex' },
+        width: '100%',
+        alignItems: { xs: 'flex-start', md: 'center' },
+        justifyContent: 'space-between',
+        maxWidth: { sm: '100%', md: '1700px' },
+        pt: 1.5,
+      }}
+      spacing={2}
+    >
+      <Stack direction="row" sx={{ gap: 1 }}>
+        <CustomDatePicker />
+      </Stack>
+    </Stack>
+  );
+}
